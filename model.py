044f0b37b6ac71bb2d2cf1ef074b73ab613e1129
Analia7 <laranalia@gmail.com>
2024-10-01 16:35:20 +0100

    create repo

---
+++ model.py
@@ -0,0 +1,17 @@
+# Importing necessary libraries
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.datasets import load_iris
+from sklearn.metrics import accuracy_score, classification_report, confusion_matrix
+from ucimlrepo import fetch_ucirepo # fetch diabetes dataset
+  
+# fetch dataset 
+cdc_diabetes_health_indicators = fetch_ucirepo(id=891) 
+  
+# data (as pandas dataframes) 
+X = cdc_diabetes_health_indicators.data.features 
+y = cdc_diabetes_health_indicators.data.targets 
+
+# Split the dataset into training and testing sets
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
