e01297c3c9e6f675b3eefd517bc8eba62baab472
Analia7 <laranalia@gmail.com>
2024-10-02 11:43:29 +0100

    Decision tree function finished

--- model.py
+++ model.py
@@ -20,7 +20,7 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_
 def test_random_forest(n_estimators, criterion, max_depth, min_samples_split, min_samples_leaf, max_features, bootstrap, n_jobs, max_samples, class_weight, oob_score):
     return
 
-def test_decision_tree(X_train, y_train, X_test, y_test, criterion, splitter, max_depth, min_samples_split, min_samples_leaf, max_features, min_impurity_decrease, class_weight, ccp_alpha, n_trees, max_leaf_nodes=2):
+def test_decision_tree(X_train, y_train, X_test, y_test, criterion, splitter, min_samples_split, min_samples_leaf, max_features, min_impurity_decrease, class_weight, ccp_alpha, n_trees, max_leaf_nodes=2):
     """
     Create a decision tree digital twin model to determine whether a patient is diabetic or not.
     Output:
@@ -32,20 +32,29 @@ def test_decision_tree(X_train, y_train, X_test, y_test, criterion, splitter, ma
     # create the decision tree model
     # n_trees decision trees will be created to deal with the randomness and the average performance will be selected
     decision_trees = np.empty(n_trees, dtype=object)
-    predictions = np.zeros(n_trees, dtype=int)
+    predictions = np.empty(n_trees)
+    max_depths = np.zeros(n_trees, dtype=int)
 
     # create and fit the tree models and use them to predict the values of y for X_test
     for i in range(n_trees):
-        decision_trees[i] = DecisionTreeClassifier(criterion=criterion, splitter=splitter, max_depth=max_depth, min_samples_split=min_samples_split,
+        decision_trees[i] = DecisionTreeClassifier(criterion=criterion, splitter=splitter, min_samples_split=min_samples_split,
                                                       min_samples_leaf=min_samples_leaf, max_features=max_features, max_leaf_nodes=max_leaf_nodes,
                                                       min_impurity_decrease=min_impurity_decrease, class_weight=class_weight, ccp_alpha=ccp_alpha)
         decision_trees[i].fit(X_train, y_train)
+        max_depths[i] = decision_trees[i].get_depth()
         predictions[i] = decision_trees[i].predict(X_test)
 
     # apply majority voting (column-wise) to get the final predicted labels
     y_pred, _ = mode(predictions, axis=0)
+    # flatten the 2D array returned by mode into a prediction 1D array
+    y_pred = y_pred.flatten()
     
+    # accuracy score
+    accuracy = accuracy_score(y_test, y_pred)
+
+    # average max_depth as a proxy for complexity/time taken
+    average_max_depth = np.mean(max_depths)
 
     #decision_trees = np.array([ for _ in range(n_trees)], dtype=object)
     
-    return
+    return (accuracy, average_max_depth)
